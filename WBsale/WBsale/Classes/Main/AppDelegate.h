15a4231331c7b28f4a28c11d720bc977d7db9143
XIAOXIANWANG <bowang0913@126.com>
2015-11-24 19:37:46 +0800

    //项目初始化

---
+++ WBsale/WBsale/Classes/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBsale
+//
+//  Created by 王博 on 15/11/24.
+//  Copyright © 2015年 王博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
