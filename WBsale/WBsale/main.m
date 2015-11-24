7a7c928207071f120837cc67f265310573459ad1
XIAOXIANWANG <bowang0913@126.com>
2015-11-24 19:35:38 +0800

    //项目初始化

---
+++ WBsale/WBsale/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WBsale
+//
+//  Created by 王博 on 15/11/24.
+//  Copyright © 2015年 王博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
